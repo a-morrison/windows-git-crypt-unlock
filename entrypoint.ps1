5e8bb16b8aeb15c01d5bbf31d9cba884961c750e
Alex Morrison <a.morrison17@gmail.com>
2019-11-18 10:41:29 -0500

    change desc

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -5,6 +5,6 @@ param (
 
 [System.Text.Encoding]::UtF8.GetString([System.Convert]::FromBase64String($GIT_CRYPT_KEY)) > ./git-crypt-key
 
-.\git-crypt.exe unlock ./git-crypt-key
+./git-crypt.exe unlock ./git-crypt-key
 
 Remove-Item ./git-crypt-key
